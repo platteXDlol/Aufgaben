f7937bab6c5591019c581e20553e43f5449519dd
platteXDlol <Pascal_Hocher@sluz.ch>
2024-10-02 13:12:25 +0200

    h

---
+++ Aufgabe_11/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diagonale_Linie_zeichnen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wie lang soll die Linie sein?");
+
+            while (true)
+            {
+                int lengthOfLine = 0;
+                if (int.TryParse(Console.ReadLine(), out lengthOfLine))
+                {
+                    DrawLine(lengthOfLine);
+                    break;
+                }
+
+                else
+                {
+                    Console.WriteLine("Ungültige Eingabe. Ganzzahl erwartet.");
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+        static void DrawLine(int length)
+        {
+            Console.WriteLine();
+
+            for (int i = 0; i < length; i++)
+            {
+                RepeatCharacter('*', i);
+                Console.Write(" ");
+                RepeatCharacter('*', length - i - 1);
+                Console.WriteLine();
+            }
+        }
+
+        static void RepeatCharacter(char character, int number)
+        {
+            for (int i = 0; i < number; i++)
+                Console.Write(character);
+
+        }
+    }
+}
+
